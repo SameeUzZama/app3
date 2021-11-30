b70b3447ec9709fc9a22652317b4252800e23dea
Samee Uz Zama <samiuzzama619@gmail.com>
2021-11-30 10:34:31 +0530

    app3

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+    return (   <div>
+            <tr>
+                <td colspan={4}>
+                    <h1>hello my friends</h1>
+                </td>
+            </tr>
+    </div>
+    )
+}
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+import "./Navigation.css"
+
+export const Navigation = () => {
+    return (
+        <div>
+            <nav>
+            <tr>
+                    <td>
+                        <a href="https://pickupbiz.com" target="blank">pickupbiz</a>
+                    </td>
+                    <td>
+                        <a href="https://google.com" target="blank">google</a>
+                    </td>
+                    <td>
+                        <a href="https://youtube.com" target="blank">youtube</a>
+                    </td>
+                    <td>
+                        <a href="https://instagram.com" target="blank">insta</a>
+                    </td>
+                </tr>
+            </nav>
+        </div>
+    );
+}
